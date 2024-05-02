a87c36ab335fc50e986f20556de4f3a6aed72818
kalyan_dahake75 <kalyan.dahake@amantyatech.com>
2024-05-02 09:06:04 +0530

    done with color chnages

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -3,9 +3,9 @@ import React from 'react'
 const Header = () => {
   return (
     <div className='flex justify-between bg-gray-200 place-items-center h-16'>
-      <div className='m-5 p-2 flex'>
-        <img className='w-7 mx-1 bg-green-400 rounded-lg' src="/logo.png" alt="image1" />
-        <span className='font-bold text-xl'>Fruit Vendor</span>
+      <div className='bg-[#FFA559] m-5 p-2 flex rounded-lg'>
+        <img className='w-7 p-1 bg-black rounded-xl' src="/logo.png" alt="image1" />
+        <span className='font-bold text-xl ml-1'>Fruit Vendor</span>
       </div>
       <div className='m-5 p-2'>Operations</div>
       <div className='flex place-items-center'>
