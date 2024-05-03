0a67b04df6e6cc22dfa548576e80f2c955a3788f
kalyan_dahake75 <kalyan.dahake@amantyatech.com>
2024-05-03 13:07:44 +0530

    done with sub cor changes

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -2,12 +2,14 @@ import React from 'react'
 
 import Carousel from "./carousel"
 import SubCarousel from "./subCarousel"
+import VendorCarousel from './vendorCarousel'
 
 const index = () => {
   return (
     <div className=''>
         <Carousel/>
         <SubCarousel/>
+        <VendorCarousel/>
     </div>
   )
 }
