423f351a175d71a089355ea71c5044722040164a
kalyan_dahake75 <kalyan.dahake@amantyatech.com>
2024-05-13 12:08:47 +0530

    done with a login header bug

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,11 +1,14 @@
 import React from 'react'
+import { useSelector } from "react-redux";
 
 import Logo from './Logo.component'
 import Sign from './Sign.component'
 
 const Header = () => {
+  const sliderStatus = useSelector((store) => store.profile.slider);
+  console.log(sliderStatus);
   return (
-    <div className='flex justify-between place-items-center h-20 px-48 sticky top-0 bg-white z-50'>
+    <div className={'flex justify-between place-items-center h-20 px-48 sticky top-0 bg-white ' + ( sliderStatus ? "" : "z-50" )}>
       <Logo/>
       <Sign/>
     </div>
