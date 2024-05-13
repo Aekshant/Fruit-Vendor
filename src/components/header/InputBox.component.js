56f1ade811426da335316e3dfe1e13598116b2ec
kalyan_dahake75 <kalyan.dahake@amantyatech.com>
2024-05-13 13:40:06 +0530

    search bar input box added

--- src/components/header/InputBox.component.js
+++ src/components/header/InputBox.component.js
@@ -1,36 +1,35 @@
-import React from 'react'
+import React, { useState } from "react";
+
+import LeftArrowSvg from "../utils/svg/LeftArrow.svg"
+import SearchSvg from "../utils/svg/Search.svg"
 
 const InputBox = () => {
+  const [open, setOpen] = useState(false);
   return (
-    <div
-      className='flex items-center'
-    >
-      <div 
-        className='bg-gray-200 flex my-5 mx-1 px-4 py-2 rounded-lg shadow-lg w-80 justify-between'
-      >
-        <span>Search For the Fruits</span>
-        <svg xmlns="http://www.w3.org/2000/svg"
-          fill="none" 
-          viewBox="0 0 24 24" 
-          strokeWidth={1} 
-          stroke="currentColor" 
-          className="w-6 h-6">
-          <path 
-            strokeLinecap="round"
-            strokeLinejoin="round" 
-            d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"
+    <div className="flex items-center">
+      {open ? (
+        <div className="flex my-5 mx-1 px-4 py-2 rounded-lg shadow-lg w-80 justify-between items-center">
+          <div  className="w-5 cursor-pointer" onClick={() => setOpen(false)}>
+            <LeftArrowSvg/>
+          </div>
+          <input
+            className="focus:outline-none placeholder-gray-500 w-full px-4"
+            placeholder="Search For the Fruits"
           />
-        </svg>
-      </div>
-      {/* 
-      <div
-        className='px-0 py-1 text-black'
-      >|</div>
-      <div
-        className='bg-[#FFA559] my-5 mx-1 px-2 py-2 text-black font-semibold rounded-lg shadow-lg'
-      >Search</div> */}
+        </div>
+      ) : (
+        <div 
+          className="bg-gray-200 flex my-5 mx-1 px-4 py-2 rounded-lg shadow-lg w-80 justify-between"
+          onClick={() => setOpen(true)}
+        >
+          <span>Search For the Fruits</span>
+          <div className="w-6 h-6">
+          <SearchSvg/>
+          </div>
+        </div>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default InputBox
+export default InputBox;
